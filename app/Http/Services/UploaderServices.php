50506715194be73e64cff3f6237511864db2db20
Karl-KLT <mo1670448@gmail.com>
2023-04-07 18:12:12 +0200

    fix image

--- app/Http/Services/UploaderServices.php
+++ app/Http/Services/UploaderServices.php
@@ -18,7 +18,7 @@ class UploaderServices
 
         // storage not stored fixed soon :)
         if (Storage::putFileAs($path,$file,$file_name)) {
-            return Storage::temporaryUrl('/assets/uploads/' . $folder . '/' . $date_path . $file_name,now()->addDays(env('MAX_DAYS_FOR_PHOTOS')));
+            return Storage::temporaryUrl('/assets/uploads/' . $folder . '/' . $date_path . $file_name,now()->addDays(env('MAX_DAYS_FOR_PHOTOS',1)));
         }
     }
 

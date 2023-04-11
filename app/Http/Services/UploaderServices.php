c11b574735f195b2091255f5a935c916aac5330c
Karl-KLT <mo1670448@gmail.com>
2023-04-12 01:27:42 +0200

    changes with fix routers and redresign ui

--- app/Http/Services/UploaderServices.php
+++ app/Http/Services/UploaderServices.php
@@ -12,14 +12,14 @@ class UploaderServices
     public function upload(UploadedFile $file, $folder)
     {
         $date_path = date("Y") . '/' . date("m") . '/' . date("d");
-        $path = '/assets/uploads/' . $folder . '/' . $date_path . '/';
+        $path = '/assets/uploads/' . $folder . '/' . $date_path;
 
         $file_name = date('YmdHis') . mt_rand() . '_' . $folder . '.' . $file->getClientOriginalExtension();
 
         // s3
         if(env('FILESYSTEM_DISK') == 's3'){
             if (Storage::putFileAs($path,$file,$file_name)) {
-                return Storage::temporaryUrl('/assets/uploads/' . $folder . '/' . $date_path . $file_name,now()->addDays(env('MAX_DAYS_FOR_PHOTOS',1)));
+                return Storage::temporaryUrl('/assets/uploads/' . $folder . '/' . $date_path . '/' . $file_name,now()->addDays(env('MAX_DAYS_FOR_PHOTOS',1)));
             }
         }
 
